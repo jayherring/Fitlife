143b535209a4823ff25b3228b171d6d04829b346
jeremyherring <j.herring.8989@gmail.com>
2017-05-16 17:07:03 -0500

    first commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnReview_Click(object sender, RoutedEventArgs e)
+        {
+            AddReview addreview = new AddReview();
+            addreview.Show();
+
+        }
+
+        private void Btnpromo_Click(object sender, RoutedEventArgs e)
+        {
+            AddPromo addpromo = new AddPromo();
+            addpromo.Show();
+        }
+    }
+}
